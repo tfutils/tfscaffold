84beeee9e9b934eaebecc656ad22a303ce3d9f4f
Ricky Barefield <ricky.barefield@bjss.com>
2020-02-25 10:28:10 +0000

    Updated to allow bootstrap to work with terraform 0.12

--- bootstrap/s3_bucket.tf
+++ bootstrap/s3_bucket.tf
@@ -29,11 +29,11 @@ resource "aws_s3_bucket" "bucket" {
 
   # This does not use default tag map merging because bootstrapping is special
   # You should use default tag map merging elsewhere
-  tags {
-    "Name"        = "Terraform Scaffold State File Bucket for account ${var.aws_account_id} in region ${var.region}"
-    "Environment" = "${var.environment}"
-    "Project"     = "${var.project}"
-    "Component"   = "${var.component}"
-    "Account"     = "${var.aws_account_id}"
+  tags = {
+    Name        = "Terraform Scaffold State File Bucket for account ${var.aws_account_id} in region ${var.region}"
+    Environment = "${var.environment}"
+    Project     = "${var.project}"
+    Component   = "${var.component}"
+    Account     = "${var.aws_account_id}"
   }
 }
